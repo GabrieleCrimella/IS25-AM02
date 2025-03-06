68b9923a14c383ed938f742e08d5a9038d264fc6
Davide Faccini <davide1.faccini@mail.polimi.it>
2025-03-06 16:02:34 +0100

    Inizio sviluppo carte

---
+++ src/main/java/it/polimi/ingsw/is25am02/model/Card.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.is25am02.model;
+
+public abstract class Card {
+    private final int level;
+
+    public Card(int level) {
+        this.level = level;
+    }
+
+    public Card newCard() {
+        return createCard();
+    }
+
+    public abstract Card createCard();
+    public abstract void effect();
+}
