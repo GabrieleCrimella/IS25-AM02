6b54d1197c6466a8e0e8f849fb2fa96a73ba1c18
Davide Faccini <davide1.faccini@mail.polimi.it>
2025-03-07 17:09:40 +0100

    Card setting

--- src/main/java/it/polimi/ingsw/is25am02/model/cards/boxes/GreenBox.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.is25am02.model.cards.boxes;
-
-import it.polimi.ingsw.is25am02.model.BoxType;
-
-public class GreenBox extends Box {
-    public GreenBox(int value, BoxType type) {
-        super(value, type);
-    }
-}
--- src/main/java/it/polimi/ingsw/is25am02/model/cards/boxes/YellowBox.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.is25am02.model.cards.boxes;
-
-import it.polimi.ingsw.is25am02.model.BoxType;
-
-public class YellowBox extends Box {
-    public YellowBox(int value, BoxType type) {
-        super(value, type);
-    }
-}
