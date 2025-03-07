c5aa821f02a6bfe6a861e4e9705f09f63ee0320a
ericadonno <erica.donno@mail.polimi.it>
2025-03-07 16:57:20 +0100

    tile checkConnectors

--- src/main/java/it/polimi/ingsw/is25am02/model/ConnectorType.java
+++ src/main/java/it/polimi/ingsw/is25am02/model/ConnectorType.java
@@ -1,5 +1,27 @@
 package it.polimi.ingsw.is25am02.model;
 
 public enum ConnectorType {
-    NONE, SINGLE, DOUBLE, UNIVERSAL
+    NONE, SINGLE, DOUBLE, UNIVERSAL;
+
+    int numConnectorType;
+
+    public int getNum() {
+        return numConnectorType;
+    }
+
+    public boolean compatible(ConnectorType other) {
+        if (this == NONE || other == NONE) {
+            return false;
+        }
+        if (this == SINGLE && other == UNIVERSAL) {
+            return true;
+        }
+        if (this == DOUBLE && other == UNIVERSAL) {
+            return true;
+        }
+        if (this == UNIVERSAL && (other == SINGLE || other == DOUBLE)) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module it.polimi.ingsw.is25am02 {
     requires javafx.controls;
     requires javafx.fxml;
-    requires static lombok;
 
 
     opens it.polimi.ingsw.is25am02 to javafx.fxml;
