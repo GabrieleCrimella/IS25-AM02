ae86ed9a64a8561f75742dda3454920eb660caad
Davide Faccini <davide1.faccini@mail.polimi.it>
2025-03-16 17:02:08 +0100

    Update

--- src/main/java/it/polimi/ingsw/is25am02/model/enumerations/StateCardType.java
+++ src/main/java/it/polimi/ingsw/is25am02/model/enumerations/StateCardType.java
@@ -1,11 +1,11 @@
 package it.polimi.ingsw.is25am02.model.enumerations;
 
 public enum StateCardType {
-    DECISION,
-    CHOICE_ATTRIBUTES,
-    REMOVE,
-    BOXMANAGEMENT,
-    ROLL,
-    FINISH
+    DECISION,    //decisioni si/no o utilizzo di batteria
+    CHOICE_ATTRIBUTES,  //scelta di cannoni e motori doppi da usare
+    REMOVE,  //rimozione di equipaggio o merce
+    BOXMANAGEMENT, //gestione carico e scarico merci
+    ROLL, //tiro dadi
+    FINISH //effetto carta finito si aspetta che il leader invochi playnextcard()
 
 }
