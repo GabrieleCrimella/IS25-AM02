b53e4fc48aa6cfd32e714a2d2fbf4d6531d1d4bb
Davide Faccini <davide1.faccini@mail.polimi.it>
2025-03-17 17:36:49 +0100

    Update Cabin and Tile

--- src/main/java/it/polimi/ingsw/is25am02/model/tiles/Tile.java
+++ src/main/java/it/polimi/ingsw/is25am02/model/tiles/Tile.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.is25am02.model.tiles;
 
+import it.polimi.ingsw.is25am02.model.Alive;
 import it.polimi.ingsw.is25am02.model.cards.boxes.Box;
 import it.polimi.ingsw.is25am02.model.enumerations.ConnectorType;
 import it.polimi.ingsw.is25am02.model.enumerations.RotationType;
@@ -9,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public sealed abstract class Tile permits BatteryStorage, BrownCabin, Cabin, Cannon, DoubleCannon, DoubleMotor, Motors, PurpleCabin, Shield, SpecialStorage, Storage, Structural{
-//todo: ricordiamoci di mettere che all'inizio della preparazione della nave i giocatori trovano già posizionata la "tile iniziale" folorata al centro della loro nave
+//todo: ricordiamoci di mettere che all'inizio della preparazione della nave i giocatori trovano già posizionata la "tile iniziale" colorata al centro della loro nave
     private TileType tType;
     private ConnectorType[] connectors;
     private RotationType rotationType;
@@ -119,49 +120,46 @@ public sealed abstract class Tile permits BatteryStorage, BrownCabin, Cabin, Can
     //metodi degli altri tipi
 
     //storage e specialStorage
-    public List<Box> getOccupation(){
-
-        List<Box> vuoto = new ArrayList<Box>();
-        return vuoto;
+    public List<Box> getOccupation()  throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
     }
 
-    public void removeBox(Box box){
+    public void removeBox(Box box) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
     }
 
-    public int getNumOccupation(){
-        return 0;
+    public int getNumOccupation() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
     }
 
-    public void addBox(Box box) {
+    public void addBox(Box box) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
     }
 
     //shield
-
-    public boolean isShielded(RotationType side){
-        return false;
+    public boolean isShielded(RotationType side) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
     }
 
-    //cabin
-    public void setAlive(int human, int brown_alien, int purple_alien){}
-
-    public int getNumHuman(){
-        return 0;
+    //cabin   //todo aggiornare cabin
+    public ArrayList<Alive> getNumCrew()  throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
     }
 
-    public int getNumPurpleAlien(){
-        return 0;
+    public void addCrew(Alive member) throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
     }
 
-    public int getNumBrownAlien(){
-        return 0;}
-
-    public void remove (int num){}
+    public void removeCrew() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
+    }
 
     //battery storage
-
-    public int getNumBattery() {
-        return 0;
+    public int getNumBattery() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
     }
 
-    public void removeBattery(){}
+    public void removeBattery() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported method");
+    }
 }
