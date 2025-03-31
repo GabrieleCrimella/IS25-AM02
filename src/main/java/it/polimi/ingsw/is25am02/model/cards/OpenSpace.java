cabe4b76bf3422d55806f6972cf42d0a6956c180
Anna Cordioli <anna.cordioli@mail.polimi.it>
2025-03-31 10:25:02 +0200

    controllo non nullità in choicedouble motor

--- src/main/java/it/polimi/ingsw/is25am02/model/cards/OpenSpace.java
+++ src/main/java/it/polimi/ingsw/is25am02/model/cards/OpenSpace.java
@@ -29,13 +29,16 @@ public class OpenSpace extends Card {
     public void choiceDoubleMotor( Game game, Player player, List<Coordinate> motors, List<Coordinate> batteries) throws IllegalRemoveException {
         List<Tile> dMotors = new ArrayList<>();
         int flyForward;
-
-        for(Coordinate motor : motors) {
-            dMotors.add(player.getSpaceship().getTile(motor.x(), motor.y()).get());
+        if(motors!=null){
+            for(Coordinate motor : motors) {
+                dMotors.add(player.getSpaceship().getTile(motor.x(), motor.y()).get());
+            }
         }
         flyForward= player.getSpaceship().calculateMotorPower(dMotors);
-        for(Coordinate battery : batteries) {
-            player.getSpaceship().getTile(battery.x(), battery.y()).get().removeBattery();
+        if(batteries!=null){
+            for(Coordinate battery : batteries) {
+                player.getSpaceship().getTile(battery.x(), battery.y()).get().removeBattery();
+            }
         }
         fly.put(player,flyForward);
         game.getGameboard().move(flyForward, player);
