4b2b9906f2bd5ce79e1abe6b6d7abec5a15d3866
Anna Cordioli <anna.cordioli@mail.polimi.it>
2025-05-11 16:56:33 +0200

    aggiunta di parametri in onupdateeverything

--- src/main/java/it/polimi/ingsw/is25am02/controller/server/GameSession.java
+++ src/main/java/it/polimi/ingsw/is25am02/controller/server/GameSession.java
@@ -23,7 +23,7 @@ public class GameSession {
         this.lobbyId = lobbyId;
         this.game = new Game(players, level);
         for(Player player : game.getPlayers()) {
-            player.onUpdateEverything(game.getlevel(), game.getPlayers(), game.getGameboard(), game.getCurrentCard(), game.getCurrentState(), player.getSpaceship().getSpaceshipIterator().getSpaceshipMask(), game.getGameboard().getStartingPosition());
+            player.onUpdateEverything(game.getlevel(), game.getPlayers(), game.getGameboard(), game.getCurrentCard(), game.getCurrentState(), player.getSpaceship().getSpaceshipIterator().getSpaceshipMask(), game.getGameboard().getStartingPosition(), game.getDeck().getDeck());
         }
         startGameQueueProcessor();
     }
