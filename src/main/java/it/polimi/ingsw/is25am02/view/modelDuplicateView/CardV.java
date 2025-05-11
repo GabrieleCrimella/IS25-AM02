0f7f71fbc951bbf89158e5ef1c5eda95bc723aca
Anna Cordioli <anna.cordioli@mail.polimi.it>
2025-05-11 16:54:53 +0200

    aggiunta commento dentro card V

--- src/main/java/it/polimi/ingsw/is25am02/view/modelDuplicateView/CardV.java
+++ src/main/java/it/polimi/ingsw/is25am02/view/modelDuplicateView/CardV.java
@@ -1,17 +1,25 @@
 package it.polimi.ingsw.is25am02.view.modelDuplicateView;
 
+import it.polimi.ingsw.is25am02.utils.enumerations.BoxType;
 import it.polimi.ingsw.is25am02.utils.enumerations.CardType;
+import it.polimi.ingsw.is25am02.utils.enumerations.RotationType;
 import it.polimi.ingsw.is25am02.utils.enumerations.StateCardType;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
 
 public class CardV {
     private StateCardType stateCard;
     private final String imagePath;
     private final CardType cardType;
+    private String comment;
 
-    public CardV(StateCardType stateCard, String imagePath, CardType cardType) {
+
+    public CardV(StateCardType stateCard, String imagePath, CardType cardType, String comment) {
         this.stateCard = stateCard;
         this.imagePath = imagePath;
         this.cardType = cardType;
+        this.comment = comment;
     }
 
     public void setStateCard(StateCardType stateCard) {
