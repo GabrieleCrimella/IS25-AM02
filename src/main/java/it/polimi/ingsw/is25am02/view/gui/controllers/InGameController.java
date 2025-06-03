4610a746e12ae790d851a51ba5c48e90aefafa94
Anna Cordioli <anna.cordioli@mail.polimi.it>
2025-06-03 13:59:13 +0200

    ✨fxml e controller per la fase di gioco

---
+++ src/main/java/it/polimi/ingsw/is25am02/view/gui/controllers/InGameController.java
@@ -0,0 +1,200 @@
+package it.polimi.ingsw.is25am02.view.gui.controllers;
+
+import it.polimi.ingsw.is25am02.utils.enumerations.PlayerColor;
+import it.polimi.ingsw.is25am02.view.modelDuplicateView.PlayerV;
+import it.polimi.ingsw.is25am02.view.modelDuplicateView.tile.TileV;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+public class InGameController extends GeneralController{
+    @FXML
+    private StackPane root;
+    @FXML
+    private Group level1Gameboard;
+    @FXML
+    private Group level2Gameboard;
+    @FXML
+    private ImageView backgroundImageView;
+    @FXML
+    private ImageView popupBackgroundImage;
+    @FXML
+    private VBox playerButtonsContainer;
+    @FXML
+    private Pane SpaceshipPane;
+    @FXML
+    private StackPane viewSpaceshipPopup;
+    @FXML private Label creditsValue;
+    @FXML private Label lostTilesValue;
+    @FXML private Label aliveValue;
+    @FXML private Label batteriesValue;
+    @FXML private Label boxesValue;
+    @FXML private ImageView MySpaceshipImage;
+
+
+    @FXML
+    public void initialize(int level, PlayerColor color) {
+        String imagePath;
+        String imagePathMySpaceship;
+        root.getStylesheets().add(
+                getClass().getResource("/style/style.css").toExternalForm()
+        );
+        switch (level) {
+            case 0:
+                imagePath = "/image/cardboard/cardboard-3.png";
+                imagePathMySpaceship = "/image/cardboard/cardboard-1.jpg";
+                break;
+            case 2:
+                imagePath = "/image/cardboard/cardboard-5.jpg";
+                imagePathMySpaceship = "/image/cardboard/cardboard-1b.jpg";
+                break;
+            default:
+                imagePath = "/image/background.jpg";
+                imagePathMySpaceship = "/image/cardboard/cardboard-1b.jpg";
+        }
+        if (level == 2) {
+            level1Gameboard.setVisible(false);
+            level1Gameboard.setManaged(false);
+            level1Gameboard.setDisable(true);
+            level2Gameboard.setVisible(true);
+            level2Gameboard.setManaged(true);
+            level2Gameboard.setDisable(false);
+
+        } else {
+            level1Gameboard.setVisible(true);
+            level1Gameboard.setManaged(true);
+            level1Gameboard.setDisable(false);
+            level2Gameboard.setVisible(false);
+            level2Gameboard.setManaged(false);
+            level2Gameboard.setDisable(true);
+        }
+
+        backgroundImageView.setImage(new Image(getClass().getResourceAsStream(imagePath)));
+        popupBackgroundImage.setImage(new Image(getClass().getResourceAsStream(imagePath)));
+        MySpaceshipImage.setImage(new Image(getClass().getResourceAsStream(imagePathMySpaceship)));
+
+        Button closeButton = new Button("x");
+        closeButton.setOnAction(e -> {
+            Platform.exit();
+            try {
+                GUIController.getInstance().getController().closeConnect();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            System.exit(0);
+        });
+
+        StackPane.setAlignment(closeButton, Pos.TOP_RIGHT);
+        StackPane.setMargin(closeButton, new Insets(10, 10, 0, 0));
+        closeButton.getStyleClass().add("closeButton");
+
+        root.getChildren().add(closeButton); // dove rootPane è il contenitore della vista
+
+        VBox temp = newNotificazionContainer();
+
+        root.getChildren().add(temp);
+        StackPane.setAlignment(temp, Pos.TOP_RIGHT);
+
+
+        List<String> playersNick = GUIController.getInstance().getController().getPlayers();
+        List<String> otherPlayers = playersNick.stream()
+                .filter(name -> !name.equals(GUIController.getInstance().getNickname()))
+                .toList();
+
+        setOtherPlayers(otherPlayers);
+    }
+
+    public void updateStats(int credits, int lostTiles, int alive, int batteries, int boxes) {
+        creditsValue.setText(String.valueOf(credits));
+        lostTilesValue.setText(String.valueOf(lostTiles));
+        aliveValue.setText(String.valueOf(alive));
+        batteriesValue.setText(String.valueOf(batteries));
+        boxesValue.setText(String.valueOf(boxes));
+    }
+
+    public void setOtherPlayers(List<String> otherPlayerNicknames) {
+        playerButtonsContainer.getChildren().clear();
+        for (Node node : SpaceshipPane.getChildren()) {
+            if (node instanceof Pane pane && pane.getId() != null && pane.getId().startsWith("cell_")) {
+                pane.getChildren().clear();
+            }
+        }
+
+        for (String nickname : otherPlayerNicknames) {
+            Button playerButton = new Button(nickname);
+            playerButton.setPrefWidth(150);
+            playerButton.setPrefHeight(51);
+            playerButton.getStyleClass().add("main-button");
+
+            // Puoi anche settare un'azione personalizzata qui
+            playerButton.setOnAction(e -> {
+                try {
+                    PlayerV playerToShow = GUIController.getInstance().getController().getPlayerVFromNickname(nickname);
+                    Optional<TileV>[][] spaceship = playerToShow.getSpaceshipBoard();
+                    for (int row = 0; row < spaceship.length; row++) {
+                        for (int col = 0; col < spaceship[row].length; col++) {
+                            Optional<TileV> tileOpt = spaceship[row][col];
+                            if (tileOpt.isPresent()) {
+                                TileV tile = tileOpt.get();
+                                StackPane tileNode = createTile(tile);
+
+                                for (Node node : SpaceshipPane.getChildren()) {
+                                    if (node instanceof Pane && node.getId() != null && node.getId().equals("cell_" + row + "_" + col)) {
+                                        ((Pane) node).getChildren().add(tileNode);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    // Mostra il popup
+                    viewSpaceshipPopup.setVisible(true);
+
+                } catch (RemoteException ex) {
+                    showNotification("Fail to get player from nickname", NotificationType.ERROR, 5000);
+                }
+
+            });
+
+            playerButtonsContainer.getChildren().add(playerButton);
+        }
+    }
+
+    private StackPane createTile(TileV tile) {
+        // Container della tile
+        StackPane tilePane = new StackPane();
+        tilePane.setPrefSize(149, 149); // stessa misura usata altrove
+        tilePane.setMinSize(149, 149);
+        tilePane.setMaxSize(149, 149);
+
+        // Crea e configura l'immagine della tile
+        ImageView imageView = new ImageView();
+        imageView.setFitWidth(149);
+        imageView.setFitHeight(149);
+        imageView.setPreserveRatio(true);
+        imageView.setImage(new Image(getClass().getResourceAsStream(tile.getImagePath())));
+
+        tilePane.getChildren().add(imageView);
+        return tilePane;
+    }
+
+
+
+}
